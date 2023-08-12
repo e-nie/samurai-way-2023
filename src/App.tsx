8f76e231214c7dd4fe09a76375155cb33a173c19
Eva Niehaus <eniehaus111@gmail.com>
2023-08-13 01:30:06 +0200

    add 19-22 lesson router 5

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,26 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 function App() {
     return (
-        // <BrowserRouter>
-        <div className = "app-wrapper">
-
-            <Header />
-            <Navbar />
-            <div className='app-wrapper-content'>
-            <Dialogs />
+        <BrowserRouter>
+            <div className = "app-wrapper">
+                <Header />
+                <Navbar />
+                <div className = 'app-wrapper-content'>
+                    <Route path={'/dialogs'} component = {Dialogs} />
+                    <Route path={'/profile'} component = {Profile} />
+                    <Route path={'/news'} component = {News} />
+                    <Route path={'/music'} component = {Music} />
+                    <Route path={'/settings'} component = {Settings} />
+                </div>
             </div>
-            {/*<Profile/>*/}
-
-
-            {/*<Route path = {'/hello'} render = {() => <HelloMessage message = 'Hello Friends!' />} />*/}
-            {/*<Route path = {'/bye'} render = {() => <ByeMessage message = 'Bye Friends!' />} />*/}
-        </div>
-        // </BrowserRouter>
+        </BrowserRouter>
     )
-        ;
 }
 
 // type MessageType = {
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,33 @@
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props: any) => {
     return (
-        <div>
-            Dialogs
+        <div className = {s.dialogs}>
+            <div className = {s.dialogsItems}>
+                <div className = {s.dialog + ' ' + s.active}>
+                    <NavLink to = {'/dialogs/1'}>Evchen</NavLink>
+                </div>
+                <div className = {s.dialog}>
+                    <NavLink to = {'/dialogs/2'}>Eloise</NavLink>
+                </div>
+                <div className = {s.dialog}>
+                    <NavLink to = {'/dialogs/3'}>Florence</NavLink>
+                </div>
+                <div className = {s.dialog}>
+                    <NavLink to = {'/dialogs/4'}>Natalie</NavLink>
+                </div>
+                <div className = {s.dialog}>
+                    <NavLink to = {'/dialogs/5'}> Isabella</NavLink>
+                </div>
+            </div>
+            <div className = {s.messages}>
+                <div className = {s.message}>Hi</div>
+                <div className = {s.message}>How is it going ?</div>
+                <div className = {s.message}>Yo</div>
+            </div>
         </div>
+
     )
 }
 
